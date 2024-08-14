6d1e093ebfa6482f364b6d1ad5a98be6092b75b9
WILLP-Keen <William@keenpayroll.ca>
2024-08-14 13:54:23 -0230

    changed auth details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import yaml
 from yaml.loader import SafeLoader
+from pathlib import Path
 import streamlit_authenticator as stauth
 
 st.title("🎈 My new app")
@@ -8,11 +9,9 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-page_names_to_funcs = {}
-
 
 def authentication():
-    with open("config.yaml") as file:
+    with open("./config.yaml") as file:
         config = yaml.load(file, Loader=SafeLoader)
 
     authenticator = stauth.Authenticate(
@@ -21,29 +20,12 @@ def authentication():
         cookie_key=config["cookie"]["key"],
         cookie_expiry_days=config["cookie"]["expiry_days"],
         pre_authorized=config["pre-authorized"],
-        auto_hash=True,
     )
-
-    print(authenticator)
 
     authenticator.login()
 
     if st.session_state["authentication_status"]:
-        col1, col2 = st.sidebar.columns(2)
-
-        col2.write("# MyKeen MVP")
-        col1.image("./PuffinLogo.ico")
-        st.sidebar.write("---")
-
-        demo_name = st.sidebar.selectbox("Clients:", page_names_to_funcs.keys())
-        authenticator.logout("↪️ Logout", "sidebar")
-
-        st.sidebar.markdown(
-            '<a href="mailto:William@keenpayroll.ca?subject=MyKeen%20Issue">Report Issue</a>',
-            unsafe_allow_html=True,
-        )
-
-        page_names_to_funcs[demo_name]()
+        st.write("Hello")
     elif st.session_state["authentication_status"] is False:
         st.error("Username/password is incorrect")
     elif st.session_state["authentication_status"] is None:
